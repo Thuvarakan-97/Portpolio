9bc24589ef3b71f8ea0395920bb2be449efb2427
thuvarakan <p.thuvarakan1997@gmail.com>
2025-03-21 17:20:14 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <SiteHeader />
-          {children}
         </ThemeProvider>
         {children}
         
